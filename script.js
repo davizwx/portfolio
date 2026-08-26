56345fdc413d6a62097799aec098010749bf7021
davizwx <davi.mandu@escola.pr.gov.br>
2026-08-26 12:11:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-let nome
-nome = prompt("qual é o seu nome?")
-alert("boas vindas " + nome)
+let nome;
+function cliquei (){
+    alert("quem clicou é gay")
+}
+nome = prompt("qual é o seu nome?");
+alert("boas vindas " + nome);
