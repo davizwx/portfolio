3b4e410d6667037f540473eb31aab4761c2f7582
davizwx <davi.mandu@escola.pr.gov.br>
2026-09-22 08:22:56 -0300

    atualização

---
+++ portfolio/atividade4/par.js
@@ -0,0 +1,16 @@
+function parouimpar(){
+    let numero;
+    
+
+
+    numero = Number(prompt("digite um numero"));
+
+
+    if(numero % 2 == 0){
+        alert("O Numero é par");
+
+    }
+    else{
+        alert("O Nuúmero é impar");
+    }
+}
