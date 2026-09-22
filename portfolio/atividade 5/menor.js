65d7eef7b02d84c2951909f2f4ebd1b4528f4231
davizwx <davi.mandu@escola.pr.gov.br>
2026-09-22 08:42:16 -0300

    nova aividade

---
+++ portfolio/atividade 5/menor.js
@@ -0,0 +1,16 @@
+function maior(){
+    let numero1;
+    let numero2;
+    let resultado
+   numero1 = Number(prompt("digite o primeiro número"));
+   numero2 = Number(prompt("Digite o segundo número"));
+   if{
+    numero1 > numero2;
+    alert("o número " + numero1 + "é maior");
+
+   }
+   else{
+    alert("O Número " + numero2 " é maior");
+   }
+
+}
