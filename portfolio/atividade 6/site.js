c3f27782e9209ab883787cb702499786f2366e55
davizwx <davi.mandu@escola.pr.gov.br>
2026-09-23 12:13:42 -0300

    atual

--- portfolio/atividade 6/site.js
+++ portfolio/atividade 6/site.js
@@ -5,7 +5,7 @@ function site() {
     
     nome = prompt("Qual é o seu nome ?");
     result = window.document.getElementById('resultado');
-    result.InnerHTML = `<p>Olá,  $(nome)! É um prazer te conhecer </p>`;
+    result.InnerHTML = `<p>Olá,  $(nome)! É um prazer te conhecer \n O sistema me enviou a seguinte informação: <mark>$(agora)<mark> </p>`;
         
     
     
